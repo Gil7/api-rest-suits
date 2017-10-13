b18b701193c750485c74426935f64964e77e642e
Gilberto Méndez <gilberto.mendez.santiz@gmail.com>
2017-10-13 14:18:23 -0500

    new fields created in the migration added to the model

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','phone','lastname','username', 'isAdmin'
+        'name', 'email', 'password','phone','lastname','username', 'isAdmin','password'
     ];
 
     /**
@@ -24,6 +24,6 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'remember_token',
     ];
 }
