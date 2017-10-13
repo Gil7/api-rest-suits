33d3fc59a2b31cb4ce57a001f33218058bc40124
Gilberto Méndez <gilberto.mendez.santiz@gmail.com>
2017-10-13 14:20:54 -0500

    seeders created to create fake data for users and products

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -15,24 +15,28 @@ class ProductsTableSeeder extends Seeder
             'name' => 'traje de prro :)',
             'description' => 'nuevo traje para todo tipo de personas',
             'price' => 199.00,
+            'rental' => 99.00,
             'stock' => 20
         ]);
         Product::create([
             'name' => 'traje de gato :)',
             'description' => 'nuevo traje para todo tipo de personas',
             'price' => 299.00,
+            'rental' => 59.00,
             'stock' => 30
         ]);
         Product::create([
             'name' => 'traje de zorro :)',
             'description' => 'nuevo traje para todo tipo de personas',
             'price' => 599.00,
+            'rental' => 199.00,
             'stock' => 3
         ]);
         Product::create([
             'name' => 'traje de pato :)',
             'description' => 'nuevo traje para todo tipo de personas',
             'price' => 159.00,
+            'rental' => 57.00,
             'stock' => 20
         ]);
     }
